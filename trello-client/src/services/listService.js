ee96d41185eda489a426c2d113152db725f2c4ef
Mason Terry <mason.terry@q2ebanking.com>
2019-10-30 09:21:26 -0500

    updating package-lock.json file

--- trello-client/src/services/listService.js
+++ trello-client/src/services/listService.js
@@ -5,7 +5,7 @@ const apiKeyAndToken = `?key=${apiKey}&token=${token}`
 
 export default {
   getLists(id) {
-    return Api.get(`/boards/${id}/lists${apiKeyAndToken}`)
+    return Promise.all([Api.get(`/boards/${id}/lists${apiKeyAndToken}`)])
   },
   addListToBoard(id, list) {
     const formattedList = list.replace(' ', '%20')
